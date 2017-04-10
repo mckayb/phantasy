c0d03b1b150df0e8b9e763a58cc2d298988d0937
McKay Broderick <mckay.db@gmail.com>
2017-04-09 22:41:26 -0600

    Curried a bunch of php functions.

--- src/Core/php-functions.php
+++ src/Core/php-functions.php
@@ -36,8 +36,8 @@ function chop()
 
 function chop2()
 {
-    return curry(function ($char_mask, $str) {
-        return \chop($str, $char_mask);
+    return curry(function ($charMask, $str) {
+        return \chop($str, $charMask);
     })(...func_get_args());
 }
 
@@ -190,5 +190,288 @@ function metaphone2()
 {
     return curry(function($phonemes, $str) {
         return \metaphone($str, $phonemes);
+    })(...func_get_args());
+}
+
+function money_format()
+{
+    return curry('\money_format')(...func_get_args());
+}
+
+function nl_langinfo()
+{
+    return curry('\nl_langinfo')(...func_get_args());
+}
+
+function nl2br()
+{
+    return curry('\nl2br')(...func_get_args());
+}
+
+function nl2br2()
+{
+    return curry(function ($isXHTML, $str) {
+        return \nl2br($str, $isXHTML);
+    })(...func_get_args());
+}
+
+function number_format()
+{
+    return curry('\number_format')(...func_get_args());
+}
+
+function number_format2()
+{
+    return curry(function($decimals, $num) {
+        return \number_format($num, $decimals);
+    })(...func_get_args());
+}
+
+function number_format4()
+{
+    return curry(function($decimals, $decPoint, $thousandsSep, $num) {
+        return \number_format($num, $decimals, $decPoint, $thousandsSep);
+    })(...func_get_args());
+}
+
+function ord()
+{
+    return curry('\ord')(...func_get_args());
+}
+
+function parse_str()
+{
+    return curry(function ($str) {
+        \parse_str($str, $output);
+        return $output;
+    })(...func_get_args());
+}
+
+function quotemeta()
+{
+    return curry('\quotemeta')(...func_get_args());
+}
+
+function rtrim()
+{
+    return curry('\rtrim')(...func_get_args());
+}
+
+function rtrim2()
+{
+    return curry(function ($charMask, $str) {
+        return \rtrim($str, $charMask);
+    })(...func_get_args());
+}
+
+function sha1_file()
+{
+    return curry('\sha1_file')(...func_get_args());
+}
+
+function sha1_file2()
+{
+    return curry(function($rawOutput, $filename) {
+        return \sha1_file($filename, $rawOutput);
+    })(...func_get_args());
+}
+
+function sha1()
+{
+    return curry('\sha1')(...func_get_args());
+}
+
+function sha12()
+{
+    return curry(function($rawOutput, $str) {
+        return \sha1($str, $rawOutput);
+    })(...func_get_args());
+}
+
+function similar_text()
+{
+    return curry('\similar_text')(...func_get_args());
+}
+
+function similar_text_pct()
+{
+    return curry(function($str1, $str2) {
+        \similar_text($str1, $str2, $percent);
+        return $percent;
+    })(...func_get_args());
+}
+
+function soundex()
+{
+    return curry('\soundex')(...func_get_args());
+}
+
+function str_getcsv()
+{
+    return curry('\str_getcsv')(...func_get_args());
+}
+
+function str_getcsv2()
+{
+    return curry(function($delim, $str) {
+        return \str_getcsv($str, $delim);
+    })(...func_get_args());
+}
+
+function str_getcsv3()
+{
+    return curry(function($delim, $enclosure, $str) {
+        return \str_getcsv($str, $delim, $enclosure);
+    })(...func_get_args());
+}
+
+function str_getcsv4()
+{
+    return curry(function($delim, $enclosure, $escape, $str) {
+        return \str_getcsv($str, $delim, $enclosure, $escape);
+    })(...func_get_args());
+}
+
+function str_ireplace()
+{
+    return curry('\str_ireplace')(...func_get_args());
+}
+
+function str_ireplace_count() {
+    return curry(function($search, $replace, $subject) {
+        \str_ireplace($search, $replace, $subject, $count);
+        return $count;
+    })(...func_get_args());
+}
+
+function str_pad()
+{
+    return curry(function($length, $str) {
+        return \str_pad($str, $length);
+    })(...func_get_args());
+}
+
+function str_pad3()
+{
+    return curry(function($length, $padStr, $str) {
+        return \str_pad($str, $length, $padStr);
+    })(...func_get_args());
+}
+
+function str_pad4()
+{
+    return curry(function($length, $padStr, $padType, $str) {
+        return \str_pad($str, $length, $padStr, $padType);
+    })(...func_get_args());
+}
+
+function str_repeat()
+{
+    return curry(function($multiplier, $str) {
+        return \str_repeat($str, $multiplier);
+    })(...func_get_args());
+}
+
+function str_replace()
+{
+    return curry('\str_replace')(...func_get_args());
+}
+
+function str_replace_count()
+{
+    return curry(function($search, $replace, $subject) {
+        \str_replace($search, $replace, $subject, $count);
+        return $count;
+    })(...func_get_args());
+}
+
+function str_rot13()
+{
+    return curry('\str_rot13')(...func_get_args());
+}
+
+function str_shuffle()
+{
+    return curry('\str_shuffle')(...func_get_args());
+}
+
+function str_split()
+{
+    return curry('\str_split')(...func_get_args());
+}
+
+function str_split2()
+{
+    return curry(function($len, $str) {
+        return \str_split($str, $len);
+    })(...func_get_args());
+}
+
+function str_word_count()
+{
+    return curry('\str_word_count')(...func_get_args());
+}
+
+function str_word_count2()
+{
+    return curry(function($format, $str) {
+        return \str_word_count($str, $format);
+    })(...func_get_args());
+}
+
+function str_word_count3()
+{
+    return curry(function($format, $charList, $str) {
+        return \str_word_count($str, $format, $charList);
+    })(...func_get_args());
+}
+
+function strcasecmp()
+{
+    return curry('\strcasecmp')(...func_get_args());
+}
+
+function strchr()
+{
+    return curry(function($needle, $haystack) {
+        return \strchr($haystack, $needle);
+    })(...func_get_args());
+}
+
+function strchr3()
+{
+    return curry(function($beforeNeedle, $needle, $haystack) {
+        return \strchr($haystack, $needle, $beforeNeedle);
+    })(...func_get_args());
+}
+
+function strcmp()
+{
+    return curry('\strcmp')(...func_get_args());
+}
+
+function strcoll()
+{
+    return curry('\strcoll')(...func_get_args());
+}
+
+function strcspn()
+{
+    return curry(function($mask, $str) {
+        return \strcspn($str, $mask);
+    })(...func_get_args());
+}
+
+function strcspn3()
+{
+    return curry(function($start, $mask, $str) {
+        return \strcspn($str, $mask, $start);
+    })(...func_get_args());
+}
+
+function strcspn4()
+{
+    return curry(function($start, $end, $mask, $str) {
+        return \strcspn($str, $mask, $start, $end);
     })(...func_get_args());
 }
--- test/PHPFunctionsTest.php
+++ test/PHPFunctionsTest.php
@@ -32,7 +32,55 @@ use function Phantasy\Core\PHP\{
     md5_file,
     md5_file2,
     md5,
-    md52
+    md52,
+    metaphone,
+    metaphone2,
+    money_format,
+    nl_langinfo,
+    nl2br,
+    nl2br2,
+    number_format,
+    number_format2,
+    number_format4,
+    ord,
+    parse_str,
+    quotemeta,
+    rtrim,
+    rtrim2,
+    sha1_file,
+    sha1_file2,
+    sha1,
+    sha12,
+    similar_text,
+    similar_text_pct,
+    soundex,
+    str_getcsv,
+    str_getcsv2,
+    str_getcsv3,
+    str_getcsv4,
+    str_ireplace,
+    str_ireplace_count,
+    str_pad,
+    str_pad3,
+    str_pad4,
+    str_repeat,
+    str_replace,
+    str_replace_count,
+    str_rot13,
+    str_shuffle,
+    str_split,
+    str_split2,
+    str_word_count,
+    str_word_count2,
+    str_word_count3,
+    strcasecmp,
+    strchr,
+    strchr3,
+    strcmp,
+    strcoll,
+    strcspn,
+    strcspn3,
+    strcspn4
 };
 
 class PHPFunctionsTest extends TestCase
@@ -299,7 +347,7 @@ class PHPFunctionsTest extends TestCase
         );
 
         $this->assertEquals(
-            \md5_file(realpath(dirname(__FILE__)) . '/fixtures/config.json'),
+            md5_file(realpath(dirname(__FILE__)) . '/fixtures/config.json'),
             '625b7290ee67ba3bb84ebe1fa5c32fbe'
         );
     }
@@ -325,5 +373,493 @@ class PHPFunctionsTest extends TestCase
     {
         $str = 'apple';
         $this->assertEquals(md52(true, $str), \md5($str, true));
+    }
+
+    function testMetaphone()
+    {
+        $this->assertEquals(metaphone('programming'), \metaphone('programming'));
+        $this->assertEquals(metaphone('programming'), 'PRKRMNK');
+    }
+
+    function testMetaphone2()
+    {
+        $this->assertEquals(metaphone2(5, 'programming'), \metaphone('programming', 5));
+        $this->assertEquals(metaphone2(5, 'programming'), 'PRKRM');
+
+        $metaphone5Phonemes = metaphone2(5);
+        $this->assertEquals($metaphone5Phonemes('programming'), 'PRKRM');
+    }
+
+    function testMoneyFormat()
+    {
+        $num = 1234.56;
+        setlocale(LC_MONETARY, 'en_IN');
+        $this->assertEquals(money_format('%i', $num), \money_format('%i', $num));
+        $this->assertEquals(money_format('%i', $num), 'INR 1,234.56');
+    }
+
+    function testNLLangInfo()
+    {
+        $this->assertEquals(nl_langinfo(MON_1), \nl_langinfo(MON_1));
+        $this->assertEquals(nl_langinfo(MON_1), "January");
+
+        $nlLangInfo = nl_langinfo();
+        $this->assertEquals($nlLangInfo(MON_1), "January");
+    }
+
+    function testNL2BR()
+    {
+        $str = "foo isn't\n bar";
+        $this->assertEquals(nl2br($str), \nl2br($str));
+        $this->assertEquals(nl2br($str), "foo isn't<br />\n bar");
+    }
+
+    function testNL2BR2()
+    {
+        $str = "Welcome\r\nThis is my HTML document";
+        $expected = "Welcome<br>\r\nThis is my HTML document";
+        $this->assertEquals(nl2br2(false, $str), \nl2br($str, false));
+
+        $this->assertEquals(nl2br2(false, $str), $expected);
+
+        $nl2brNotXHtml = nl2br2(false);
+        $this->assertEquals($nl2brNotXHtml($str), $expected);
+    }
+
+    function testNumberFormat()
+    {
+        $num = 1234.56;
+        $this->assertEquals(number_format($num), \number_format($num));
+        $this->assertEquals(number_format($num), '1,235');
+
+        $numFormat = number_format();
+        $this->assertEquals($numFormat($num), '1,235');
+    }
+
+    function testNumberFormat2()
+    {
+        $num = 1234.56;
+        $this->assertEquals(number_format2(2, $num), \number_format($num, 2));
+        $this->assertEquals(number_format2(2, $num), '1,234.56');
+
+        $formatTo2Decimals = number_format2(2);
+        $this->assertEquals($formatTo2Decimals($num), '1,234.56');
+    }
+
+    function testNumberFormat4()
+    {
+        $num = 1234.56;
+        $this->assertEquals(number_format4(2, ',', ' ', $num), \number_format($num, 2, ',', ' '));
+        $this->assertEquals(number_format4(2, ',', ' ', $num), '1 234,56');
+
+        $frNumberFormat = number_format4(2, ',', ' ');
+        $this->assertEquals($frNumberFormat($num), '1 234,56');
+    }
+
+    function testOrd()
+    {
+        $str = "\n";
+        $this->assertEquals(ord($str), \ord($str));
+        $this->assertEquals(ord($str), 10);
+
+        $ord = ord();
+        $this->assertEquals($ord($str), 10);
+    }
+
+    function testParseStr()
+    {
+        $str = "first=value&arr[]=foo+bar&arr[]=baz";
+        $output = parse_str($str);
+        \parse_str($str, $expected);
+        $this->assertEquals($output, $expected);
+    }
+
+    function testQuotemeta()
+    {
+        $str = "Hello world. (can you hear me?)";
+        $expected = "Hello world\. \(can you hear me\?\)";
+        $this->assertEquals(quotemeta($str), \quotemeta($str));
+        $this->assertEquals(quotemeta($str), $expected);
+
+        $quotemeta = quotemeta();
+        $this->assertEquals($quotemeta($str), $expected);
+    }
+
+    function testRTrim()
+    {
+        $this->assertEquals(rtrim("test\n"), \rtrim("test\n"));
+        $this->assertEquals(rtrim("test\n"), "test");
+
+        $rtrim = rtrim();
+        $this->assertEquals($rtrim("test\n"), "test");
+    }
+
+    function testRTrim2()
+    {
+        $this->assertEquals(rtrim2("World!", "Hello World!"), \rtrim("Hello World!", "World!"));
+        $this->assertEquals(rtrim2("World!", "Hello World!"), "Hello ");
+
+        $rtrimWorld = rtrim2("World!");
+        $this->assertEquals($rtrimWorld("Hello World!"), "Hello ");
+    }
+
+    function testSHA1File()
+    {
+        $this->assertEquals(
+            sha1_file(realpath(dirname(__FILE__)) . '/fixtures/config.json'),
+            \sha1_file(realpath(dirname(__FILE__)) . '/fixtures/config.json')
+        );
+
+        $this->assertEquals(
+            sha1_file(realpath(dirname(__FILE__)) . '/fixtures/config.json'),
+            '7b25c66924c90da0ffd45fa1e78f858d37cae7bc'
+        );
+    }
+
+    function testSHA1File2()
+    {
+        $this->assertEquals(
+            sha1_file2(false, realpath(dirname(__FILE__)) . '/fixtures/config.json'),
+            \sha1_file(realpath(dirname(__FILE__)) . '/fixtures/config.json', false)
+        );
+
+        $sha1File2NoRaw = sha1_file2(false);
+        $this->assertEquals(
+            $sha1File2NoRaw(realpath(dirname(__FILE__)) . '/fixtures/config.json'),
+            \sha1_file(realpath(dirname(__FILE__)) . '/fixtures/config.json', false)
+        );
+    }
+
+    function testSha1()
+    {
+        $str = 'apple';
+        $expected = 'd0be2dc421be4fcd0172e5afceea3970e2f3d940';
+
+        $this->assertEquals(sha1($str), \sha1($str));
+        $this->assertEquals(sha1($str), $expected);
+
+        $sha1 = sha1();
+        $this->assertEquals($sha1($str), $expected);
+    }
+
+    function testSha12()
+    {
+        $str = 'apple';
+        $this->assertEquals(sha12(true, $str), \sha1($str, true));
+    }
+
+    function testSimilarText()
+    {
+        $a = 'This is a test!';
+        $b = 'A test, this is!';
+        $this->assertEquals(similar_text($a, $b), \similar_text($a, $b));
+        $this->assertEquals(similar_text($a, $b), 7);
+
+        $similarToA = similar_text($a);
+        $this->assertEquals($similarToA($b), 7);
+    }
+
+    function testSimilarTextPct()
+    {
+        $a = 'This is a test!';
+        $b = 'A test, this is!';
+        $sim = \similar_text($a, $b, $pct);
+        $expected = 45.16129032258065;
+        $this->assertEquals(similar_text_pct($a, $b), $pct);
+        $this->assertEquals(similar_text_pct($a, $b), $expected);
+
+        $similarToA = similar_text_pct($a);
+        $this->assertEquals($similarToA($b), $expected);
+    }
+
+    function testSoundex()
+    {
+        $str = 'Euler';
+        $expected = 'E460';
+        $this->assertEquals(soundex($str), \soundex($str));
+        $this->assertEquals(soundex($str), $expected);
+
+        $soundex = soundex();
+        $this->assertEquals($soundex($str), $expected);
+    }
+
+    function testStrGetCSV()
+    {
+        $str = 'a,b';
+        $this->assertEquals(str_getcsv($str), \str_getcsv($str));
+        $this->assertEquals(str_getcsv($str), ['a', 'b']);
+
+        $strGetCSV = str_getcsv();
+        $this->assertEquals($strGetCSV($str), ['a', 'b']);
+    }
+
+    function testStrGetCSV2()
+    {
+        $str = 'a.b';
+        $this->assertEquals(str_getcsv2('.', $str), \str_getcsv($str, '.'));
+        $this->assertEquals(str_getcsv2('.', $str), ['a', 'b']);
+
+        $getCSVDotSep = str_getcsv2('.');
+        $this->assertEquals($getCSVDotSep($str), ['a', 'b']);
+    }
+
+    function testStrGetCSV3()
+    {
+        $str = '&a&.&b&';
+        $this->assertEquals(str_getcsv3('.', '&', $str), \str_getcsv($str, '.', '&'));
+        $this->assertEquals(str_getcsv3('.', '&', $str), ['a', 'b']);
+
+        $getCSVDotSep = str_getcsv3('.');
+        $getCSVDotSepAmpEnclosure = $getCSVDotSep('&');
+        $this->assertEquals($getCSVDotSepAmpEnclosure($str), ['a', 'b']);
+    }
+
+    function testStrGetCSV4()
+    {
+        $str = '&^a&.&^b&';
+        $this->assertEquals(str_getcsv4('.', '&', '^', $str), \str_getcsv($str, '.', '&', '^'));
+        $this->assertEquals(str_getcsv4('.', '&', '^', $str), ['^a', '^b']);
+
+        $getCSVDotSepAmpEnclosureCaretEscape = str_getcsv4('.', '&', '^');
+        $this->assertEquals($getCSVDotSepAmpEnclosureCaretEscape($str), ['^a', '^b']);
+    }
+
+    function testStrIReplace()
+    {
+        $str = 'This is a test!';
+        $this->assertEquals(str_ireplace('!', '.', $str), \str_ireplace('!', '.', $str));
+        $this->assertEquals(str_ireplace('!', '.', $str), 'This is a test.');
+
+        $quiet = str_ireplace('!', '.');
+        $this->assertEquals($quiet($str), 'This is a test.');
+    }
+
+    function testStrIReplaceCount()
+    {
+        $str = 'This is a test!';
+        \str_ireplace('!', '.', $str, $count);
+        $this->assertEquals(str_ireplace_count('!', '.', $str), $count);
+        $this->assertEquals(str_ireplace_count('!', '.', $str), 1);
+
+        $quiet = str_ireplace_count('!', '.');
+        $this->assertEquals($quiet($str), 1);
+    }
+
+    function testStrPad()
+    {
+        $str = 'Alien';
+        $this->assertEquals(str_pad(6, $str), \str_pad($str, 6));
+        $this->assertEquals(str_pad(6, $str), 'Alien ');
+
+        $padTo6 = str_pad(6);
+        $this->assertEquals($padTo6($str), 'Alien ');
+    }
+
+    function testStrPad3()
+    {
+        $str = 'Alien';
+        $this->assertEquals(str_pad3(6, '-', $str), \str_pad($str, 6, '-'));
+        $this->assertEquals(str_pad3(6, '-', $str), 'Alien-');
+
+        $padTo6 = str_pad3(6);
+        $padTo6WithDash = $padTo6('-');
+        $this->assertEquals($padTo6WithDash($str), 'Alien-');
+    }
+
+    function testStrPad4()
+    {
+        $str = 'Alien';
+        $this->assertEquals(str_pad4(7, '-', STR_PAD_LEFT, $str), \str_pad($str, 7, '-', STR_PAD_LEFT));
+        $this->assertEquals(str_pad4(7, '-', STR_PAD_LEFT, $str), '--Alien');
+
+        $padTo7 = str_pad4(7);
+        $padTo7WithDashOnTheLeft = $padTo7('-', STR_PAD_LEFT);
+        $this->assertEquals($padTo7WithDashOnTheLeft($str), '--Alien');
+    }
+
+    function testStrRepeat()
+    {
+        $this->assertEquals(str_repeat(2, 'a'), \str_repeat('a', 2));
+        $this->assertEquals(str_repeat(2, 'a'), 'aa');
+
+        $repeatTwice = str_repeat(2);
+        $this->assertEquals($repeatTwice('a'), 'aa');
+    }
+
+    function testStrReplace()
+    {
+        $str = 'This is a test!';
+        $this->assertEquals(str_replace('!', '.', $str), \str_replace('!', '.', $str));
+        $this->assertEquals(str_replace('!', '.', $str), 'This is a test.');
+
+        $quiet = str_replace('!', '.');
+        $this->assertEquals($quiet($str), 'This is a test.');
+    }
+
+    function testStrReplaceCount()
+    {
+        $str = 'This is a test!';
+        \str_replace('!', '.', $str, $count);
+        $this->assertEquals(str_replace_count('!', '.', $str), $count);
+        $this->assertEquals(str_replace_count('!', '.', $str), 1);
+
+        $quiet = str_replace_count('!', '.');
+        $this->assertEquals($quiet($str), 1);
+    }
+
+    function testStrRot13()
+    {
+        $str = 'PHP 4.3.0';
+        $expected = 'CUC 4.3.0';
+        $this->assertEquals(str_rot13($str), \str_rot13($str));
+        $this->assertEquals(str_rot13($str), $expected);
+
+        $strRot13 = str_rot13();
+        $this->assertEquals($strRot13($str), $expected);
+    }
+
+    function testStrShuffle()
+    {
+        $str = 'abcdefg';
+        $this->assertEquals(strlen(str_shuffle($str)), strlen(\str_shuffle($str)));
+
+        $shuffle = str_shuffle();
+        $this->assertEquals(strlen($shuffle($str)), strlen(\str_shuffle($str)));
+    }
+
+    function testStrSplit()
+    {
+        $str = 'abc';
+        $this->assertEquals(str_split($str), \str_split($str));
+        $this->assertEquals(str_split($str), ['a', 'b', 'c']);
+
+        $strSplit = str_split();
+        $this->assertEquals($strSplit($str), ['a', 'b', 'c']);
+    }
+
+    function testStrSplit2()
+    {
+        $str = 'abc';
+        $this->assertEquals(str_split2(2, $str), \str_split($str, 2));
+        $this->assertEquals(str_split2(2, $str), ['ab', 'c']);
+
+        $strSplit2 = str_split2(2);
+        $this->assertEquals($strSplit2($str), ['ab', 'c']);
+    }
+
+    function testStrWordCount()
+    {
+        $str = 'This is a test.';
+        $this->assertEquals(str_word_count($str), \str_word_count($str));
+        $this->assertEquals(str_word_count($str), 4);
+
+        $wc = str_word_count();
+        $this->assertEquals($wc($str), 4);
+    }
+
+    function testStrWordCount2()
+    {
+        $str = 'This is a test.';
+        $this->assertEquals(str_word_count2(1, $str), \str_word_count($str, 1));
+        $this->assertEquals(str_word_count2(1, $str), [ 'This', 'is', 'a', 'test' ]);
+
+        $words = str_word_count2(1);
+        $this->assertEquals($words($str), ['This', 'is', 'a', 'test']);
+    }
+
+    function testStrWordCount3()
+    {
+        $str = 'This is a test.';
+        $this->assertEquals(str_word_count3(0, "\ ", $str), \str_word_count($str, 0, "\ "));
+        $this->assertEquals(str_word_count3(0, "\ ", $str), 1);
+
+        $wc = str_word_count3(0, "\ ");
+        $this->assertEquals($wc($str), 1);
+    }
+
+    function testStrCaseCmp()
+    {
+        $a = 'a';
+        $b = 'b';
+        $this->assertEquals(strcasecmp($a, $b), \strcasecmp($a, $b));
+        $this->assertEquals(strcasecmp($a, $b), -1);
+
+        $cmpWitha = strcasecmp($a);
+        $this->assertEquals($cmpWitha($b), -1);
+    }
+
+    function testStrChr()
+    {
+        $email  = 'name@example.com';
+        $this->assertEquals(strchr('@', $email), \strchr($email, '@'));
+        $this->assertEquals(strchr('@', $email), '@example.com');
+
+        $searchForAt = strchr('@');
+        $this->assertEquals($searchForAt($email), '@example.com');
+    }
+
+    function testStrChr3()
+    {
+        $email  = 'name@example.com';
+        $this->assertEquals(strchr3(true, '@', $email), \strchr($email, '@', true));
+        $this->assertEquals(strchr3(true, '@', $email), 'name');
+
+        $searchForAt = strchr3(true, '@');
+        $this->assertEquals($searchForAt($email), 'name');
+    }
+
+    function testStrCmp()
+    {
+        $a = 'a';
+        $b = 'b';
+        $this->assertEquals(strcmp($a, $b), \strcmp($a, $b));
+        $this->assertEquals(strcmp($a, $b), -1);
+
+        $cmpWitha = strcmp($a);
+        $this->assertEquals($cmpWitha($b), -1);
+    }
+
+    function testStrColl()
+    {
+        $a = 'a';
+        $b = 'b';
+        $this->assertEquals(strcoll($a, $b), \strcoll($a, $b));
+        $this->assertEquals(strcoll($a, $b), -1);
+
+        $cmpWitha = strcoll($a);
+        $this->assertEquals($cmpWitha($b), -1);
+    }
+
+    function testStrcspn()
+    {
+        $a = 'abcd';
+        $b = 'apple';
+
+        $this->assertEquals(strcspn($b, $a), \strcspn($a, $b));
+
+        $strcspnB = strcspn($b);
+        $this->assertEquals($strcspnB($a), \strcspn($a, $b));
+    }
+
+    function testStrcspn3()
+    {
+        $a = 'abcdhelloabcd';
+        $b = 'abcd';
+        $this->assertEquals(strcspn3(-9, $b, $a), \strcspn($a, $b, -9));
+        $this->assertEquals(strcspn3(-9, $b, $a), 5);
+
+        $f = strcspn3(-9);
+        $this->assertEquals($f($b, $a), \strcspn($a, $b, -9));
+    }
+
+    function testStrcspn4()
+    {
+        $a = 'abcdhelloabcd';
+        $b = 'abcd';
+        $this->assertEquals(strcspn4(-9, -5, $b, $a), \strcspn($a, $b, -9, -5));
+        $this->assertEquals(strcspn4(-9, -5, $b, $a), 4);
+
+        $f = strcspn4(-9, -5);
+        $this->assertEquals($f($b, $a), \strcspn($a, $b, -9, -5));
     }
 }
